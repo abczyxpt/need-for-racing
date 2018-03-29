4fcc87d60272d735c173724b645d64199352a8ee
abczyxpt <abczyxpt@gmail.com>
2018-03-29 18:26:30 +0800

    20180329Clinent

---
+++ Scripts/ServerConnect/PhotonClientConnect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonClientConnect{
+
+    private static PhotonClientConnect instance = null;
+
+    public static PhotonClientConnect Get
+    {
+        get { return instance ?? (instance = new PhotonClientConnect()); }
+    }
+    
+}

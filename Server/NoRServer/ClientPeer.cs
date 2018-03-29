63c47f1223e00dcd1da926c7fcbb69ebf669fe5e
abczyxpt <abczyxpt@gmail.com>
2018-03-29 14:46:32 +0800

    dll create

---
+++ Server/NoRServer/ClientPeer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace NoRServer
+{
+    /// <summary>
+    /// 用于管理客户端的连接问题
+    /// </summary>
+    public class ClientPeer : Photon.SocketServer.ClientPeer
+    {
+        public ClientPeer(InitRequest initRequest) : base(initRequest)
+        {
+
+        }
+        
+
+        //服务器断开后工作
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            
+        }
+
+        //服务器启动工作
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            
+        }
+    }
+}
---
+++ Server/NoRServer/NoRServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+
+namespace NoRServer
+{
+
+    public class NoRServer : ApplicationBase
+    {
+
+        /// <summary>
+        /// 当一个客户端请求连接
+        /// </summary>
+        /// <param name="initRequest"></param>
+        /// <returns></returns>
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new ClientPeer(initRequest);
+        }
+
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        protected override void Setup()
+        {
+
+        }
+
+
+        /// <summary>
+        /// server关闭
+        /// </summary>
+        protected override void TearDown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
